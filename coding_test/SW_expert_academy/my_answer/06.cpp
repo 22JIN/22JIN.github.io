25fe5f04a87c2d29a613a0c3ae5f0bc37cd9d222
22JIN <k22jin@gmail.com>
2022-04-20 21:04:19 +0900

    test

--- coding_test/SW_expert_academy/my_answer/06.cpp
+++ coding_test/SW_expert_academy/my_answer/06.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
 #include <stdio.h>
-
-
 using namespace std;
 
+
+int main(int argc, char** argv)
+{
+	int test_case = 0;
+	int T = 0;
+
+	freopen("input.txt", "r", stdin);
+	cin >> T;
+ 
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        int day = 0;
+        cin >> day;        
+        int nums[day];
+        
+        for(int i=0; i<day; i++)
+            cin >> nums[i];
+        
+        int max = nums[day-1];
+        int sum = 0;
+        for(int i=day-2; i>=0; i--){
+            if(max > nums[i])
+                sum += max-nums[i];
+            else if(max < nums[i])
+                max = nums[i];
+           // cout << "nums[" << i<< "]: " << nums[i] << ", max: " << max << ", sum: " << sum << endl;
+        }
+  
+        cout << "#" << test_case << " " << sum << endl;
+	}
+	return 0;
+}
+
+
+/* useless due to speed (maybe)...
 //find Max from index day
 int findMax(int* cost, int index, int day){
     int max = 0;
     int max_index = 0;
     for(int i=index; i<day; i++){
-        if(cost[i] > max){
+        if(cost[i] >= max){
             max = cost[i];
             max_index = i;
         }
@@ -28,48 +61,34 @@ int main(int argc, char** argv)
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
         int day = 0;
-        cin >> day;
-        cout << "day: " << day << endl;
-        
+        cin >> day;        
         int nums[day];
 
         for(int i=0; i<day; i++)
             cin >> nums[i];
             
+        int start_day = 0;
         int max_day = 0;
-        int max_day1 = 0;
-        
         int sum = 0;
-        int count = 0;
+
         while(1){
-            count ++;
-            if(count == 10)
-                break;
-
             int benefit = 0;
-            max_day1 = findMax(nums, max_day, day);
-            cout << "maxday1: " << max_day1 << ",maxday: " << max_day << endl;
-            
-            if(max_day == max_day1 )
-                break;
-            
-            for(int i=0; i<(day-max_day1); i++){
-                benefit =+ nums[i];
-            }
-            cout << "benefit: " << benefit << endl;
-            benefit = nums[max_day1]*(max_day1-max_day-1) - benefit; 
-            sum =+ benefit;
+            max_day = findMax(nums, start_day, day);
 
-            if(max_day1 == day-1 )
-                break;
+            //add price between start day and max day
+            for(int i=start_day; i<max_day; i++)
+                benefit += nums[i];
             
-            
+            benefit = nums[max_day]*(max_day-start_day) - benefit; 
+            sum += benefit;
+            start_day = max_day+1;
+
+            //finish searching max day
+            if(max_day == day-1 )
+                break;            
         }
-
         cout << "#" << test_case << " " << sum << endl;
-
-
 	}
 	return 0;
-    
 }
+*/
