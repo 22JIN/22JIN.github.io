64a2e356db674eba048097e462a1925fb58a5f17
22JIN <k22jin@gmail.com>
2022-06-18 18:10:45 +0900

    20220618_1810

--- SW_expert_academy/my_answer/18.cpp
+++ SW_expert_academy/my_answer/18.cpp
@@ -2,6 +2,47 @@
 #include <string.h>
 using namespace std;
 
+
+int main(int argc, char** argv){
+
+    int T = 0;
+	freopen("input.txt", "r", stdin);
+	cin >> T;
+
+	for(int test_case = 1; test_case <= T; ++test_case)
+	{
+        int num = 0;
+        cin >> num;
+
+        string num_str = to_string(num);
+        int len = num_str.length();
+        string num_min = num_str;
+        string num_max = num_str;
+
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+                string tmp = num_str;
+                if(i==0 && (int)num_str[j]=='0')
+                    continue;
+                if(j==0 && (int)num_str[i]=='0')
+                    continue;
+
+                tmp[i] = num_str[j];
+                tmp[j] = num_str[i];
+                if(tmp.compare(num_min) < 0)
+                    num_min = tmp;
+                if(tmp.compare(num_max) > 0)
+                    num_max = tmp;
+
+            }
+        }
+        cout << "#" << test_case << " " << num_min << " " << num_max << endl;
+	}
+    
+	return 0;
+}
+
+/*
 int findMax(string str, int start_index, int end_index){
     int max = 0;
     int index_max = 0;
@@ -27,6 +68,8 @@ int findMin(string str, int start_index, int end_index){
     }
     return index_min;
 }
+
+
 
 int main(int argc, char** argv)
 {
@@ -130,7 +173,7 @@ int main(int argc, char** argv)
 }
 
 
-/*
+
         for(int i=0; i<len; i++){
             index_min = findMin(num_min, i, len);
             //cout << "i: " << i << ", index_min: " << index_min << endl;
